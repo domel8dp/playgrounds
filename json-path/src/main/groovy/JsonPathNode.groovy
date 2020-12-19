24e7dda253e9de8afebcfca84680621f8cbec30b
Dominik Pawlak <domel8dp@yahoo.co.uk>
2020-12-20 00:03:48 +0100

    Additional null / missing tests

--- json-path/src/main/groovy/JsonPathNode.groovy
+++ json-path/src/main/groovy/JsonPathNode.groovy
@@ -9,6 +9,7 @@ def JSON = '''
     "str": "some text",
     "pi" : 3.14159265,
     "flag": true,
+    "not_set": null,
     "array": [
         {"active":true, "selected":true},
         {"active":true, "selected":false}
@@ -31,14 +32,22 @@ printClassAndValue(json, '$.int')
 printClassAndValue(json, '$.str')
 printClassAndValue(json, '$.pi')
 printClassAndValue(json, '$.flag')
+printClassAndValue(json, '$.not_set')
+printClassAndValue(json, '$.missing')
 printClassAndValue(json, '$.array.[0]')
 printClassAndValue(json, '$.array.[*].selected')
+printClassAndValue(json, '$.array.[*].missing')
 def jsonArray = jsonParser.parse(JSON_ARRAY)
 printClassAndValue(jsonArray, '$.[*].selected')
+printClassAndValue(jsonArray, '$.[*].missing')
 
 static def printClassAndValue(json, path) {
-    println "### ${path}"
-    def data = json.read(path)
-    println data.getClass()
-    println data
+    try {
+        println "### ${path}"
+        def data = json.read(path)
+        println data.getClass()
+        println data
+    } catch (Exception e) {
+        e.printStackTrace()
+    }
 }
